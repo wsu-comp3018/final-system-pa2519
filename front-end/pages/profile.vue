6beedc3889337ae4ca4c5b4ccc0a283d4f5fcc43
Luan <22025530@student.westernsydney.edu.au>
2025-05-04 17:04:40 +1000

    Front-end

---
+++ front-end/pages/profile.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="h-full">
+        <div class="flex items-center gap-6 h-fit p-10 bg-gray-500">
+            <div class="w-2/6">
+                <div class="flex justify-end">
+                    <img class="w-[170px] h-[170px] text-center border-2 border-white rounded-full">
+                </div>
+            </div>
+            <div class="grow text-[30px]">
+                <h1>John Smith</h1>
+                <h2>Insert_Job_Position</h2>
+                <p>Email: JohnSmith@email.com</p>
+            </div>
+
+        </div>
+
+        <div class="flex justify-center h-1/2 m-5 gap-5">
+            <div class="bg-gray-500 w-1/5">
+
+            </div>
+
+            <div class="bg-gray-500 grow">
+
+            </div>
+            
+            <div class="bg-gray-500 w-1/5">
+
+            </div>
+        </div>
+
+    </div>
+</template>
---
+++ front-end/pages/signup.vue
@@ -0,0 +1,46 @@
+<style scoped>
+    input[type="text"], input[type="password"], input[type="email"]{
+        @apply border-2 border-gray-500 w-full h-[40px] bg-black mb-4 mt-2 py-6 indent-4;
+    }
+
+    input[type="password"]{
+        @apply my-2
+    }
+
+    input[type="submit"]{
+        @apply border-2 border-gray-500 rounded-full w-full mt-[20px] p-[10px] bg-gray-500;
+    }
+</style>
+
+<template>
+    
+    <div class="h-full p-10">
+        <h1 class="text-center text-[35px] p-5"><b>Create Your Account</b></h1>
+
+        <div class="h-fit w-[450px] bg-[#222222] rounded-xl mx-auto">
+
+            <div class="p-10">
+                <form>
+                    <div class="flex justify-center gap-5">
+                        <div>
+                            <label for="fname">FIRST NAME</label>
+                            <input type="text" id="fname" name="fname" placeholder="First Name">
+                        </div>
+                        <div>
+                            <label for="lname">LAST NAME</label>
+                            <input type="text" id="lname" name="lname" placeholder="Last Name">
+                        </div>    
+                    </div>
+                    <label for="email">EMAIL ADDRESS</label>
+                    <input type="email" id="email" name="email" placeholder="Email">
+
+                    <label for="password">PASSWORD</label>
+                    <input type="password" id="password" name="password" placeholder="Password">
+
+                    <input type="submit" value="Sign Up">
+                </form>
+            </div>
+
+        </div>
+    </div>
+</template>
