52a5ec0d36274bb679e9d264ca3ddecee7180a05
Devarshi-Joshi <22058010@student.westernsydney.edu.au>
2025-05-06 10:30:29 +1000

    learning-2

--- djangobackend/core/core/urls.py
+++ djangobackend/core/core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import *
 
 urlpatterns = [
+    path('',home,name='home'),
+    path('success-page/',success_page,name='success_page'),
     path('admin/', admin.site.urls),
 ]
