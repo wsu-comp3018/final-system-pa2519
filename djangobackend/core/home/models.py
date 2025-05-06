bcb566663425edd7db6db3464610bf81886ed800
Devarshi-Joshi <22058010@student.westernsydney.edu.au>
2025-05-06 12:46:43 +1000

    change

--- djangobackend/core/home/models.py
+++ djangobackend/core/home/models.py
@@ -72,3 +72,28 @@ class Statements(models.Model):
     audio_transcript=models.TextField(null=True, blank=True)
     updated_content=models.TextField(null=True, blank=True)
 
+class Sessions(models.Model):
+         id = models.IntegerField()
+         case_id = models.IntegerField()
+         user_id = models.IntegerField()
+         interviewee_id = models.IntegerField()
+         template_id = models.IntegerField()
+         session_name = models.CharField(max_length=250)
+         session_date = models.DateField
+ 
+class AudioRecordings(models.Model):
+    id = models.IntegerField()
+    session_id = models.IntegerField()
+    statement_id = models.IntegerField()
+    audio_name = models.CharField(max_length(250))
+    length = models.DurationField()
+    size = models.IntegerField()
+    audio_transcript = models.TextField(blank=True, null=True)
+    audio_path = models.FileField(upload_to='audio/', blank=True, null=True)
+    # audio_path=models.TextField(blank=True, null=True)
+ 
+class StatementTemplates(models.Model):
+    id = models.IntegerField()
+    slug = models.SlugField(unique=True, max_length=250)
+    name = models.CharField(max_length=250)
+
