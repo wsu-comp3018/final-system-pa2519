89c1d9c610b7949f67983cb290ac799a4b7311d5
Luan <22025530@student.westernsydney.edu.au>
2025-05-06 12:31:50 +1000

    removed @apply and replace with normal css for login and signup pages

--- front-end/pages/signup.vue
+++ front-end/pages/signup.vue
@@ -1,12 +1,24 @@
 <style scoped>
     input[type="text"], input[type="password"], input[type="email"]{
-        @apply border-2 border-gray-500 w-full h-[40px] bg-black mb-4 mt-2 py-6 indent-4;
+        border-width: 2px;
+        border-color: #6b7280;
+        width: 100%;
+        height: 40px;
+        background-color: black;
+        margin-bottom: 16px;
+        margin-top: 8px;
+        padding: 24px 0 24px 0;
+        text-indent: 16px;
     }
 
-    
-
-    input[type="submit"]{
-        @apply border-2 border-gray-500 rounded-full w-full mt-[20px] p-[10px] bg-gray-500;
+    input[type="submit"] {
+        border-width: 2px;
+        border-color: #6b7280;
+        width: 100%;
+        margin-top: 20px;
+        padding: 10px;
+        background-color: #6b7280;
+        border-radius: 9999px;
     }
 </style>
 
